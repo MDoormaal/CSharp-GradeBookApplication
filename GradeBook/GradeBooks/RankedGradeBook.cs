bfd8565850208d624c15b0f1418a2967fd5c3a70
MDoormaal <47192025+MDoormaal@users.noreply.github.com>
2020-07-18 14:18:30 +0200

    Override GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -14,5 +14,41 @@ namespace GradeBook.GradeBooks
         {
             base.Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (base.Students.Count < 5)
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+
+            int dropNumber = base.Students.Count / 5;
+
+            var orderedStudents = base.Students
+                    .OrderByDescending(s => s.AverageGrade)
+                    .ToList();
+
+            char grade = 'A';
+
+            for (int i = 1; i <= base.Students.Count; i++)
+            {
+                if (orderedStudents[i-1].AverageGrade == averageGrade)
+                    return grade;
+
+                if (i % dropNumber == 0)
+                {
+                    switch (grade)
+                    {
+                        case 'A': grade = 'B'; break;
+                        case 'B': grade = 'C'; break;
+                        case 'C': grade = 'D'; break;
+                        case 'D': grade = 'F'; break;
+                    }
+                }
+
+                if (grade == 'F')
+                    break;
+            }
+
+            return 'F';
+        }
     }
 }
